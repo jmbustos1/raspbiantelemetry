3d9b35174b5313d0b5c6e81c84c6434f1095f768
Luis Saavedra <luis94855510@gmail.com>
2020-01-06 19:07:40 -0300

    emify fix

--- install.sh
+++ install.sh
@@ -3,26 +3,25 @@
 # sim7600
 aptitude install libqmi-utils udhcpc gpsd
 
-qmicli -p -d /dev/cdc-wdm0 --wds-modify-profile="3gpp,1,pdp-type=IPV4"
-qmicli -p -d /dev/cdc-wdm0 --wds-modify-profile="3gpp2,1,pdp-type=IPV4"
-qmicli -p -d /dev/cdc-wdm0 --wds-delete-profile="3gpp2,1"
-qmicli -p -d /dev/cdc-wdm0 --wds-set-default-profile-num="3gpp,1"
-# qmicli -p -d /dev/cdc-wdm0 --wds-set-default-profile-num="3gpp2,1"
-qmicli -p -d /dev/cdc-wdm0 --wds-set-autoconnect-settings=enabled
-qmicli -p -d /dev/cdc-wdm0 --wds-set-ip-family=4
+while [ $(ls /dev/cdc-wdm* 2> /dev/null| wc -l) = 0 ]
+do
+  echo "waiting for /dev/cdc-wdm* ..."
+  sleep 1
+done
 
-cp etc/systemd/system/set7600.service /etc/systemd/system/set7600.service
-cp usr/bin/set7600 /usr/bin/set7600
-systemctl enable set7600
+devwwan0="$(ls /dev/cdc-wdm*| tail -n 1| awk '{print $1}')"
+
+qmicli -p -d ${devwwan0} --wds-modify-profile="3gpp,1,pdp-type=IPV4"
+qmicli -p -d ${devwwan0} --wds-set-ip-family=4
+qmicli -p -d ${devwwan0} --wds-set-default-profile-num="3gpp,1"
+qmicli -p -d ${devwwan0} --wds-delete-profile="3gpp2,1"
+qmicli -p -d ${devwwan0} --wds-set-autoconnect-settings=disabled
 
 cp usr/bin/check7600 /usr/bin/check7600
 cat crontab.root | crontab -
 
 # CAN
 cat boot/config.txt >> /boot/config.txt
-cp etc/systemd/system/setMCP2515.service /etc/systemd/system/setMCP2515.service
-cp usr/bin/setMCP2515 /usr/bin/setMCP2515
-systemctl enable setMCP2515
 
 # python
 aptitude install virtualenv
