5fbb1b39bdd694c13f7d0300da75c0b3d661bc71
Luis Saavedra <luis94855510@gmail.com>
2020-01-09 22:41:21 +0000

    fix git pull

--- pull.sh
+++ pull.sh
@@ -6,16 +6,20 @@ ACTIVATE_PATH="$(dirname ${WORKING_DIR})/env3/bin/activate"
 cd ${WORKING_DIR}
 source ${ACTIVATE_PATH}
 
-git pull > pull.log 2>&1
-
-if [ $(cat pull.log|grep "Already up-to-date."|wc -l) -ne 1 ] &&
-   [ $(cat pull.log|grep "Ya está actualizado."|wc -l) -ne 1 ] &&
-   [ $(cat pull.log|grep "Name or service not known"|wc -l) -ne 1 ]
+git remote update
+if [ $(git status -uno| grep 'origin/master'| grep -v 'up to date'| wc -l) -ge 1 ]
 then
-  pip3 install -r requirements.txt
-  sudo cp cancollecter.conf /etc/supervisor/conf.d/cancollecter.conf
-  sudo cp push2aws.conf /etc/supervisor/conf.d/push2aws.conf
-  sudo service supervisor restart
-  sudo cp can0 /etc/network/interfaces.d/can0
-  sudo service networking restart
+  git pull > pull.log 2>&1
+
+  if [ $(cat pull.log|grep "Already up-to-date."|wc -l) -ne 1 ] &&
+     [ $(cat pull.log|grep "Ya está actualizado."|wc -l) -ne 1 ] &&
+     [ $(cat pull.log|grep "Name or service not known"|wc -l) -ne 1 ]
+  then
+    pip3 install -r requirements.txt
+    sudo cp cancollecter.conf /etc/supervisor/conf.d/cancollecter.conf
+    sudo cp push2aws.conf /etc/supervisor/conf.d/push2aws.conf
+    sudo service supervisor restart
+    sudo cp can0 /etc/network/interfaces.d/can0
+    sudo service networking restart
+  fi
 fi
