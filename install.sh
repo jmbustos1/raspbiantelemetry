19862fa969ce17bfb3263760c2dcd0acc41d4dab
Luis Saavedra <luis94855510@gmail.com>
2020-01-09 15:38:59 +0000

    fix gps enabled

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+WORKING_DIR="$(dirname $(readlink -f ${0}))"
+
 # sim7600
-aptitude install libqmi-utils udhcpc gpsd supervisor
+aptitude install libqmi-utils udhcpc gpsd
 
 while [ $(ls /dev/cdc-wdm* 2> /dev/null| wc -l) = 0 ]
 do
@@ -17,22 +19,24 @@ qmicli -p -d ${devwwan0} --wds-set-default-profile-num="3gpp,1"
 qmicli -p -d ${devwwan0} --wds-delete-profile="3gpp2,1"
 qmicli -p -d ${devwwan0} --wds-set-autoconnect-settings=disabled
 
-cp usr/bin/check7600 /usr/bin/check7600
-cat crontab.root | crontab -
+cp ${WORKING_DIR}/usr/bin/check7600 /usr/bin/check7600
+cat ${WORKING_DIR}/crontab.root| crontab -
 
 # CAN
-cat boot/config.txt >> /boot/config.txt
-cp can0 /etc/network/interfaces.d/can0
-mkdir -p No_Enviados
-mkdir -p Enviados
+sed -i '/###############CAN ENABLED#############/,/############CAN ENABLED END############/d' /boot/config.txt
+cat ${WORKING_DIR}/boot/config.txt >> /boot/config.txt
+cp ${WORKING_DIR}/can0 /etc/network/interfaces.d/can0
+mkdir -p ${WORKING_DIR}/No_Enviados
+mkdir -p ${WORKING_DIR}/Enviados
 
 # python
-aptitude install virtualenv
+aptitude install virtualenv supervisor
 
-su - pi -c '
+su - pi -c "
 virtualenv -p python3 env3
 source env3/bin/activate
 pip3 install --upgrade pip
-pip3 install -r raspbiantelemetry/requirements.txt
-deactivate
-'
+pip3 install -r ${WORKING_DIR}/requirements.txt
+"
+
+su - pi -c "cat ${WORKING_DIR}/crontab.user| crontab -"
