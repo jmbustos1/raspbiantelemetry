161b314f8c3a329828cbc70cf32669b4327e519f
jmbustos1 <jmbustos1@uc.cl>
2020-01-17 16:53:18 -0300

    10 to 40 seg of data

--- CANSOC1_respaldo.py
+++ CANSOC1_respaldo.py
@@ -170,7 +170,7 @@ async def printData():
                 #print(GPS_data)
                 print(TO_SEND)
 
-                await asyncio.sleep(10)
+                await asyncio.sleep(40)
 
 
 async def main():
