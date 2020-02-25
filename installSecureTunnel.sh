55432b88886fcec8913c9e389a89ebc2baff6742
Luis Saavedra <luis94855510@gmail.com>
2020-02-25 18:03:28 +0000

    fix mkdir

--- installSecureTunnel.sh
+++ installSecureTunnel.sh
@@ -5,6 +5,7 @@ cp etc/default/secure-tunnel.conf /etc/default/secure-tunnel.conf
 cp etc/systemd/system/secure-tunnel@.service /etc/systemd/system/secure-tunnel@.service
 
 # add ocpp.voltiosc.cl to list of known hosts
+su -c "mkdir -p ~/.ssh"
 su -c "ssh-keyscan -t rsa -H ocpp.voltiosc.cl >> ~/.ssh/known_hosts"
 
 # start daemon
